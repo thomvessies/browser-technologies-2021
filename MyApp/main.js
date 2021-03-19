d26787177c83ec490a3198c11220448a5b1aaeb4
thomvessies <t.vessies98@gmail.com>
2021-03-19 09:41:54 +0100

    eerste commit

---
+++ MyApp/main.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Using static files from static directory
+app.use(express.static('public'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
+app.use('/css', express.static(__dirname + 'public/css'));
+
+// Setting views (EJS)
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  
+});
+
+app.listen(port, () => console.log(`App is running on port ${port}`));
