7943c01329363fe36ca63bf7633b86fcde39a26a
thomvessies <t.vessies98@gmail.com>
2021-04-01 11:02:39 +0200

    Finished localStorage

--- MyApp/public/js/script.js
+++ MyApp/public/js/script.js
@@ -1,73 +1,37 @@
-console.log('hello world');
+console.log(localStorage)
+
+if (document.getElementById('studentGegevens')){
+    formNaam = 'studentGegevens'
+}
+else if (document.getElementById('WAFS')){
+    formNaam = 'WAFS'
+}
+else if (document.getElementById('CSSTTR')){
+    formNaam = 'CSSTTR'
+}
+else if (document.getElementById('BT')){
+    formNaam = 'BT'
+}
+
+retrieveStudentGegevens(formNaam)
 
 if (document.getElementById('studentButton')){
     let studentButton = document.getElementById('studentButton')
     studentButton.addEventListener('click', e => storeStudentGegevens(e))
 }
 
-if (document.getElementById('invulButton')){
-    let invulButton = document.getElementById('invulButton')
-    invulButton.addEventListener('click', e => retreiveStudentGegevens(e))
-}
-
-
 if (document.getElementById('formButton')){
     let formButton = document.getElementById('formButton')
-    formButton.addEventListener('click', e => storeFormGegevens(e))
-    console.log('check')
+    formButton.addEventListener('click', e => storeFormGegevens(e, formNaam))
 }
-
-
-
-
-
-// if (document.querySelector('#naam').value == ""){
-//     const forms = document.querySelectorAll('form').values;
-//     const form = forms[0];
-
-//     Array.from(form.elements).forEach((input) => {
-//         console.log(input);
-//     });
-//     retreiveStudentGegevens()
-// }
-
-
-
-
-
-
 
 function storeStudentGegevens(e){
     // console.log(e)
     // localStorage.clear()
     var nameValue = document.querySelector('#naam').value
-    var stdnrValue = document.querySelector('#stdnr').value
     localStorage.setItem('naam', nameValue)
+    var stdnrValue = document.querySelector('#stdnr').value
     localStorage.setItem('stdnr', stdnrValue)
-    
-}
-
-function retreiveStudentGegevens(){
-    console.log('check')
-    var naamValue = localStorage.getItem('naam')
-    var stdnrValue = localStorage.getItem('stdnr')
-    console.log(naamValue)
-    document.getElementById('naam').value = naamValue
-    document.getElementById('stdnr').value = stdnrValue
-    // if (localStorage.getItem('docent')){
-    //     console.log(localStorage.getItem('docent').value)
-    //     document.getElementById(localStorage.getItem('docent').value).checked = true
-    // }
-    // if (localStorage.getItem('mening')){
-    //     document.getElementById(localStorage.getItem('mening').value).checked = true
-    // }
-    // if (localStorage.getItem('moeilijk')){
-    //     document.getElementById(localStorage.getItem('moeilijk').value).checked = true
-    // }
-    // if (localStorage.getItem('uitleg')){
-    //     document.getElementById(localStorage.getItem('uitleg').value).checked = true
-    // }
-
 }
 
 function storeFormGegevens(e){
@@ -76,31 +40,79 @@ function storeFormGegevens(e){
     localStorage.setItem('naam', nameValue)
     var stdnrValue = document.querySelector('#stdnr').value
     localStorage.setItem('stdnr', stdnrValue)
-    console.log(document.getElementsByName('docent'))
+    var startweekValue = document.querySelector('#startweek').value
+    var eindweekValue = document.querySelector('#eindweek').value
     document.getElementsByName('docent')
         .forEach(radio => {
             if (radio.checked){
-                localStorage.setItem('docent', radio.id)
+                formDocent = radio.id
             }
         })
     document.getElementsByName('mening')
         .forEach(radio => {
             if (radio.checked){
-                localStorage.setItem('mening', radio.id)
+                formMening = radio.id
             }
         })
     document.getElementsByName('moeilijk')
         .forEach(radio => {
             if (radio.checked){
-                localStorage.setItem('moeilijk', radio.id)
+                formMoeilijk = radio.id
             }
         })
     document.getElementsByName('uitleg')
         .forEach(radio => {
             if (radio.checked){
-                localStorage.setItem('uitleg', radio.id)
+                formUitleg = radio.id
             }
         })
-    console.log(localStorage)
+    var feedbackValue = document.querySelector('#feedback').value
+    formData = {startweek: startweekValue, eindweek: eindweekValue, docent: formDocent, mening: formMening, moeilijk: formMoeilijk, uitleg: formUitleg, feedback: feedbackValue}
+    formJSON = JSON.stringify(formData)
+    localStorage.setItem(formNaam, formJSON)
 }
 
+function retrieveStudentGegevens(formNaam){
+    var naamValue = localStorage.getItem('naam')
+    var stdnrValue = localStorage.getItem('stdnr')
+    document.getElementById('naam').value = naamValue
+    document.getElementById('stdnr').value = stdnrValue
+}
+
+function retrieveFormGegevens(formNaam){
+    formJSON = localStorage.getItem(formNaam)
+    formData = JSON.parse(formJSON)
+    
+    document.getElementById('startweek').value = formData.startweek
+    document.getElementById('eindweek').value = formData.eindweek
+
+
+    document.getElementsByName('docent')
+        .forEach(radio => {
+            if (radio.id == formData.docent){
+                radio.checked = true
+            }
+        })
+    document.getElementsByName('mening')
+        .forEach(radio => {
+            if (radio.id == formData.mening){
+                radio.checked = true
+            }
+        })
+    document.getElementsByName('moeilijk')
+        .forEach(radio => {
+            if (radio.id == formData.moeilijk){
+                radio.checked = true
+            }
+        })
+    document.getElementsByName('uitleg')
+        .forEach(radio => {
+            if (radio.id == formData.uitleg){
+                radio.checked = true
+            }
+        })
+    
+    
+}
+
+retrieveFormGegevens(formNaam)
